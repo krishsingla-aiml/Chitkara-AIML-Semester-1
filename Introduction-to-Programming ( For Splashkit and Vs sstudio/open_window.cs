fc24efed668a2cd164c4394efcf8af350b8fa570
krishsingla-aiml <krishksingla@gmail.com>
2026-09-05 14:52:44 +0530

    Added SplashKit open window code

---
+++ Introduction-to-Programming ( For Splashkit and Vs sstudio/open_window.cs
@@ -0,0 +1,16 @@
+﻿using SplashKitSDK;
+using static SplashKitSDK.SplashKit;
+int width;
+WriteLine("Enter width ");
+width=Convert.ToInt32(ReadLine());
+int height;
+WriteLine("Enter height ");
+height=Convert.ToInt32(ReadLine());
+Window window=OpenWindow("Krish testing window", width,height);
+ClearScreen(ColorPurple());
+RefreshScreen();
+while(!QuitRequested());
+
+
+
+
