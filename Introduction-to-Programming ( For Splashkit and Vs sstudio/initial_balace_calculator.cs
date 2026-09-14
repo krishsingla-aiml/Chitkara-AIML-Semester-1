c7c1aca1f5e5d9d432bdc6ec2e8ab64e24d1d929
krishsingla-aiml <krishksingla@gmail.com>
2026-09-14 18:13:51 +0530

    initial_balance-calculator

---
+++ Introduction-to-Programming ( For Splashkit and Vs sstudio/initial_balace_calculator.cs
@@ -0,0 +1,26 @@
+﻿using static SplashKitSDK.SplashKit;
+string name;
+WriteLine("Welcome to our bank appliciation");
+WriteLine("Enter your name?Enter name");
+name= ReadLine();
+float initialbalance;
+WriteLine("Enter your initial balance?Enter initial balance");
+initialbalance=Convert.ToInt32(ReadLine());
+float transactionamount;
+WriteLine("Enter transaction amount?Enter amount");
+transactionamount=Convert.ToInt32(ReadLine());
+float addtransactionamount;
+addtransactionamount=initialbalance+transactionamount;
+if (transactionamount<1000)
+{
+    WriteLine("Invalid transaction");
+    WriteLine("Note:-Enter amount more than 1000");
+}
+else
+{
+WriteLine("Your account summary... " +name);
+WriteLine("Your initial balance is " +initialbalance);
+WriteLine("Transaction amount is " +transactionamount);
+WriteLine("Your Current balance is "+addtransactionamount);
+WriteLine("Thanku for using....");
+}
