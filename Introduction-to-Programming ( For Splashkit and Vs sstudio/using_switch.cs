5a7f51156cf76f084fe77b3d9ef76ee9a5da9dfd
krishsingla-aiml <krishksingla@gmail.com>
2026-09-16 18:15:03 +0530

    using_switch

---
+++ Introduction-to-Programming ( For Splashkit and Vs sstudio/using_switch.cs
@@ -0,0 +1,9 @@
+﻿using static SplashKitSDK.SplashKit;
+OpenWindow("Test window",400,300);
+while(! QuitRequested())
+{
+    FillCircle(RandomColor(),Rnd(400),Rnd(300),Rnd(50));
+    RefreshScreen();
+
+    ProcessEvents();
+}
